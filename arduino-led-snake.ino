c228d4dbee2df9b9735ffd6be469d8bc25a30d94
SALES <Adrien.Sales@ville-noumea.nc>
2018-12-09 11:45:52 +1100

    Some comment in the header/formating
    Fixes #1

--- arduino-led-snake.ino
+++ arduino-led-snake.ino
@@ -1,25 +1,22 @@
-  // led setup
-  // total amount of leds
-  const int NB_LEDS = 6;
+  /*
+     Source code repo : https://github.com/adriens/arduino-led-snake/
+	 
+  */
+  const int NB_LEDS = 6;                    // total amount of leds
+  const int LED_STEP = 2;                   // step between two pins : 2 means 1/2
+  const int LED_START_POS = 2;              // pin of the first led
 
-  // step between two pins : 2 means 1/2
-  const int LED_STEP = 2;
-  // pin of the first led
-  const int LED_START_POS = 2;
-
-  // time setup
-  const int DELAY_BETWEEN_TWO_LEDS = 100;
-  const int DELAY_BETWEEN_TWO_LOOPS = 100;
+  const int DELAY_BETWEEN_TWO_LEDS = 100;   // time setup : time between two leds
+  const int DELAY_BETWEEN_TWO_LOOPS = 100;  // time setup : time between two loops
 
   int pinLed[NB_LEDS];
 
 
   void setup()
   {
-    //int pinLed[nbLeds];
     Serial.begin(9600);
-    // assign led positions to pinLeds
-    Serial.println("Setup ...");
+    
+    Serial.println("Setup ...");            // assign led positions to pinLeds
     Serial.println("Filling leds positions ...");
     int ledPos = LED_START_POS;
     for (int i=0; i < NB_LEDS; i++){
@@ -33,7 +30,6 @@
      }
     Serial.println("Leds positions set.");
 
-    // setUp all leds in output mode
     Serial.println("Putting leds to OUTPUT mode...");
     for (int i=0; i < NB_LEDS; i++){
         pinMode(pinLed[i], OUTPUT);
@@ -44,9 +40,9 @@
   {
     for (int i=0; i <= NB_LEDS; i++){
         digitalWrite(pinLed[i], HIGH);
-        delay(DELAY_BETWEEN_TWO_LEDS); // Wait for 1000 millisecond(s)
+        delay(DELAY_BETWEEN_TWO_LEDS);      // Wait for 1000 millisecond(s)
         digitalWrite(pinLed[i], LOW);
-        delay(DELAY_BETWEEN_TWO_LEDS); // Wait for 1000 millisecond(s)
+        delay(DELAY_BETWEEN_TWO_LEDS);      // Wait for 1000 millisecond(s)
      }
     Serial.print("Waiting <");
     Serial.print(DELAY_BETWEEN_TWO_LOOPS);
